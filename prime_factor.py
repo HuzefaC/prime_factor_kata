c0c8d3dbedef14af234285713479d32994abc48e
HuzefaC <huzefachabukswar@gmail.com>
2022-05-08 23:45:17 +0530

    Pass Test for prime factors of 9

--- prime_factor.py
+++ prime_factor.py
@@ -1,9 +1,9 @@
 def get_prime_factors(number):
   prime_factors = []
-  if number > 1:
-    while number % 2 == 0:
-      prime_factors.append(2)
-      number = number // 2
-    if number > 1:
-      prime_factors.append(number)
+  divisor = 2
+  while number > 1:
+    while number % divisor == 0:
+      prime_factors.append(divisor)
+      number = number // divisor
+    divisor += 1
   return prime_factors
