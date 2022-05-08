08deee6dbd87abbbfa07c47ba11f5f3af6b8e0d8
HuzefaC <huzefachabukswar@gmail.com>
2022-05-08 23:31:48 +0530

    Pass Test for prime factor of 4

--- prime_factor.py
+++ prime_factor.py
@@ -1,4 +1,9 @@
 def get_prime_factors(number):
+  prime_factors = []
   if number > 1:
-    return [number]
-  return []
+    if number % 2 == 0:
+      prime_factors.append(2)
+      number = number // 2
+    if number > 1:
+      prime_factors.append(number)
+  return prime_factors
