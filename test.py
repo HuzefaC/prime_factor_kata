ca220d9fd9d849a29ae99e148be26c0619519bf2
HuzefaC <huzefachabukswar@gmail.com>
2022-05-08 22:51:25 +0530

    Test for prime factor of 2

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ class TestPrimeFactor(unittest.TestCase):
   def test_prime_factor_of_1(self):
     self.assertListEqual(get_prime_factors(1), [])
 
+  def test_prime_factor_of_2(self):
+    self.assertListEqual(get_prime_factors(2), [2])
+
 
 if __name__=='__main__':
   unittest.main()
