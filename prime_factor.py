93443d49bbc1cbbf6a5142ae73a5934201b3d329
HuzefaC <huzefachabukswar@gmail.com>
2022-05-08 23:38:04 +0530

    Pass Test for prime factors of 8

--- prime_factor.py
+++ prime_factor.py
@@ -1,7 +1,7 @@
 def get_prime_factors(number):
   prime_factors = []
   if number > 1:
-    if number % 2 == 0:
+    while number % 2 == 0:
       prime_factors.append(2)
       number = number // 2
     if number > 1:
